5e2a89ddb826296d572bf73fb5b452a021c655b6
xayoteh <javier.act080298@gmail.com>
2023-12-22 17:43:49 -0600

    4.2

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,8 +1,10 @@
-﻿using System.Text;
+﻿using System.Reflection;
+using System.Text;
 internal class Program
 {
     private static void Main(string[] args)
     {
+        var cardCount = new Dictionary<int, int>();
         var filename = "input.txt";
         var sum = 0;
         using(var file = new StreamReader(filename))
@@ -10,28 +12,38 @@ internal class Program
             string? card;
             while((card = file.ReadLine()) != null) 
             {
-                sum += EvaluateCard(card);
+                var (number, matches) = EvaluateCard(card);
+                
+                cardCount.TryGetValue(number, out int count);
+                cardCount[number] = count + 1;
+                for(int i = 1; i <= matches; ++i) 
+                {
+                    cardCount.TryGetValue(number + i, out count);
+                    cardCount[number + i] = count + cardCount[number];
+                }
             }
+        }
+        foreach(var count in cardCount)
+        {
+            sum += count.Value;
         }
         Console.WriteLine(sum);
     }
 
-    private static int EvaluateCard(string _card) 
+    private static (int number, int matches) EvaluateCard(string _card) 
     {
         var card = new StringReader(_card.Replace("Card ", ""));
-        var cardNumber = GetNumber(card);
+        var cardNumber = int.Parse(GetNumber(card));
         var winningNumbers = GetNumbers(card);
         var myNumbers = new HashSet<int>(GetNumbers(card));
-        int points = 0;
+        int matches = 0;
 
         foreach(var number in winningNumbers) 
         {
-            if(!myNumbers.Contains(number)) continue;
-            if (points == 0) ++points;
-            else points *= 2;
+            if(myNumbers.Contains(number)) ++matches;
         }
 
-        return points;
+        return (cardNumber, matches);
     }
 
     private static string GetNumber(StringReader card)
