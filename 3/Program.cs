3f2a77bfbb033bc62267441cf1f134ee4f263549
xayoteh <javier.act080298@gmail.com>
2023-12-22 15:39:23 -0600

    3.1

---
+++ 3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Data;
+using System.IO;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var engine = new List<char[]>();
+        var filename = "input.txt";
+        int sum;
+        using (var file = new StreamReader(filename))
+        {
+            string? line;
+            while((line = file.ReadLine()) != null)
+                engine.Add(line.ToCharArray());
+        }
+        sum = Evaluate(engine);
+        Console.WriteLine(sum);
+    }
+
+    private static int Evaluate(List<char[]> engine)
+    {
+        int sum = 0;
+
+        for(int row = 0; row < engine.Count; ++row)
+        {
+            for(int col = 0; col < engine[row].Length; ++col)
+            {
+                if(engine[row][col] != '.' && !char.IsDigit(engine[row][col]))
+                    sum += GetParts(engine, row, col);
+            }
+        }
+
+        return sum;
+    }
+
+    private static int GetParts(List<char[]> engine, int row, int col)
+    {
+        int parts = 0;
+
+        for(int i = row - 1; i <= row + 1; i++) 
+        {
+            for(int j = col - 1; j <= col + 1; j++)
+            {
+                if(i == row && j == col) continue;
+                if(i < 0 || i >= engine.Count) continue;
+                if(j < 0 || j >= engine[row].Length) continue;
+                if(GetNumber(engine[i], j, out int num))
+                {
+                    parts += num;
+                }
+            }
+        }
+
+        return parts;
+    }
+
+    private static bool GetNumber(char[] line, int col, out int number) 
+    {
+        if(!char.IsDigit(line[col])) 
+        {
+            number = 0;
+            return false;
+        }
+
+        var num = new StringBuilder();
+
+        for(int i = col; i < line.Length && char.IsDigit(line[i]); ++i)
+        {
+            num.Append(line[i]);
+            line[i] = '.';
+        }
+
+        for(int i = col - 1; i >= 0 && char.IsDigit(line[i]); --i)
+        {
+            num.Insert(0, line[i]);
+            line[i] = '.';
+        }
+
+        number = int.Parse(num.ToString());
+        return true;
+    }
+}
