b9451c76a16320b99cec656f4409cc0acfe04da5
xayoteh <javier.act080298@gmail.com>
2023-12-22 15:28:52 -0600

    1.2

--- 1/Program.cs
+++ 1/Program.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 internal class Program
 {
+    private static readonly Dictionary<string, string> map = new Dictionary<string, string>() {
+        {"one", "1"}, {"two", "2"}, {"three", "3"}, {"four", "4"}, {"five", "5"},
+        {"six", "6"}, {"seven", "7"}, {"eight", "8"}, {"nine", "9"}
+    };
     private static readonly char[] digits = "123456789".ToArray();
     private static void Main(string[] args)
     {
@@ -21,10 +25,25 @@ internal class Program
         }
     }
 
-    private static string GetNumber(string line) {
-        int firstIdx = line.IndexOfAny(digits), 
-            lastIdx = line.LastIndexOfAny(digits);
+    private static string GetNumber(string s) {
+        int firstIdx = s.IndexOfAny(digits), 
+            lastIdx = s.LastIndexOfAny(digits);
+        string firstDigit = $"{(firstIdx != -1 ? s[firstIdx]:"")}", 
+            lastDigit = $"{(lastIdx != -1 ? s[lastIdx]:"")}";
+        
+        foreach(var kvp in map) {
+            int idx = s.IndexOf(kvp.Key);
+            if(firstIdx == -1 || (idx != -1 && idx < firstIdx)) {
+                firstIdx = idx;
+                firstDigit = kvp.Value;
+            }
+            idx = s.LastIndexOf(kvp.Key);
+            if(idx > lastIdx) {
+                lastIdx = idx;
+                lastDigit = kvp.Value;
+            }
+        }
 
-        return $"{line[firstIdx]}{line[lastIdx]}";
+        return firstDigit + lastDigit;
     }
 }
